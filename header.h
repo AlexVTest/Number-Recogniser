0a6a8a10d5ae49137f3d424f65a3d5840f371356
AlexVTest <vihlayew@gmail.com>
2016-07-30 12:06:31 +0300

    Mover declarations
    Moved functions and variables declaration to the header file.

--- header.h
+++ header.h
@@ -15,4 +15,18 @@
 
 using namespace std;
 
+int data_In[30][30];                                                            // Input data array
+int DB[10][30][30];                                                             // Data from DB
+int sameless_arr[10];                                                           // Arr of sameless values
+int true_Number;                                                                // True num
+
+void load_Data_In();                                                            // To load input data
+void load_DB(int number);                                                       // To load DB
+void check_Mono_Array(int arr[10]);                                             //   Functions
+void check_Dual_Array(int arr[30][30]);                                         //   for printing
+void check_Triple_Array(int arr[10][30][30], int elem);                         //   arrays to the terminal
+void check_Sameless(int number);                                                // To check sameless
+int detect_number();                                                            // To detect true number
+void save_Changes_To_DB(int number);                                            // To save learning process
+
 #endif /* header_h */
--- main.cpp
+++ main.cpp
@@ -6,21 +6,7 @@
 //  Copyright © 2016 Alex. All rights reserved.
 //
 
-#include "header.h"                                                             // Importing all libraries in header
-
-int data_In[30][30];                                                            // Input data array
-int DB[10][30][30];                                                             // Data from DB
-int sameless_arr[10];                                                           // Arr of sameless values
-int true_Number;                                                                // True num
-
-void load_Data_In();                                                            // To load input data
-void load_DB(int number);                                                       // To load DB
-void check_Mono_Array(int arr[10]);                                             //   Functions
-void check_Dual_Array(int arr[30][30]);                                         //   for printing
-void check_Triple_Array(int arr[10][30][30], int elem);                         //   arrays to the terminal
-void check_Sameless(int number);                                                // To check sameless
-int detect_number();                                                            // To detect true number
-void save_Changes_To_DB(int number);                                            // To save learning process
+#include "header.h"                                                             // Importing all libraries and setting variables in header
 
 
 int main() {
